8c7d8281d854078e282a3f33a6b641f5068ff38e
Calgooon <calhounjohn@icloud.com>
2015-09-03 10:11:24 -0400

    update

--- picCounter.js
+++ picCounter.js
@@ -2,7 +2,7 @@ var count = 0;
 
 function update() {
 if(count == 0) {
-document.getElementById("pic").src="abby.jpg";
+document.getElementById("pic").src="me.jpg";
 document.getElementById("pic").style.height="100%";
 document.getElementById("pic").style.width="100%";
 document.getElementById("pic").style.align="left"; }
@@ -11,18 +11,20 @@ document.getElementById("pic").src="champs.jpg";
 document.getElementById("pic").style.height="100%";
 document.getElementById("pic").style.width="100%"; }
 if(count == 2) {
-document.getElementById("pic").src="first.jpg"; }
+document.getElementById("pic").src="abby.jpg"; 
+document.getElementById("pic").style.height="100%";
+document.getElementById("pic").style.width="100%"; }
 }
 
 
 function clickLeft() {
 count--;
-if(count < 0) {count = 1;}
+if(count < 0) {count = 2;}
 update();
 }
 
 function clickRight() {
 count++;
-if(count > 1) {count = 0;}
+if(count > 2) {count = 0;}
 update();
 }
